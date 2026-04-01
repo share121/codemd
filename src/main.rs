52e9c69bd5955ca329af96681af1105ea9ae720b
share121 <me@s121.top>
2026-04-01 12:04:13 +0800

    feat: 优化代码

--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,54 @@ use path_clean::PathClean;
 use std::env;
 
 fn main() {
-    let mut clipboard = Clipboard::new().expect("无法获取剪贴板");
     let mut args = env::args();
     args.next();
     let exts = args.next().expect("你需要传入类似 js,rs,html 的扩展名列表");
     let exts: Vec<_> = exts.split(",").map(|s| s.trim()).collect();
     let current_dir = env::current_dir().unwrap();
-    let mut dirs = Vec::new();
+    let mut entries = Vec::new();
     for arg in args {
-        dirs.push(current_dir.join(arg));
+        entries.push(current_dir.join(arg));
     }
-    if dirs.is_empty() {
-        dirs.push(current_dir.clone());
+    if entries.is_empty() {
+        entries.push(current_dir.clone());
     }
     let relative_dir = current_dir.parent().unwrap_or(&current_dir).to_path_buf();
     let mut md = String::new();
     let mut count = 0;
-    while let Some(dir) = dirs.pop() {
-        let entries = fs::read_dir(dir).unwrap();
-        for entry in entries {
-            let entry = entry.unwrap();
-            let path = entry.path();
-            let file_type = entry.file_type().unwrap();
-            if file_type.is_dir() {
-                dirs.push(path);
-                continue;
-            }
-            let ext = path
+    while let Some(entry) = entries.pop() {
+        if entry.is_file() {
+            let ext = entry
                 .extension()
-                .unwrap_or_default()
-                .to_str()
+                .map(|e| e.to_string_lossy())
                 .unwrap_or_default();
-            if !exts.contains(&ext) {
+            if !exts.contains(&ext.as_ref()) {
                 continue;
             }
-            let relative_path = path.strip_prefix(&relative_dir).unwrap_or(&path).clean();
+            let relative_path = entry.strip_prefix(&relative_dir).unwrap_or(&entry).clean();
             println!("发现 {}", relative_path.display());
             md += &format!(
-                "```{} title=\"{}\"\n{}\n```\n\n",
+                "```{} path=\"{}\"\n{}\n```\n\n",
                 ext,
                 relative_path.display(),
-                fs::read_to_string(&path).unwrap().trim_end()
+                fs::read_to_string(&entry).unwrap().trim_end()
             );
             count += 1;
+        } else if entry.is_dir() {
+            let Ok(dir) = fs::read_dir(entry).inspect_err(|e| eprintln!("{e}")) else {
+                continue;
+            };
+            entries.extend(dir.flatten().map(|e| e.path()));
         }
     }
     println!("共生成 {} 个代码块", count);
-    clipboard.set_text(md).expect("写入剪贴板失败");
-    println!("已将代码块复制到剪贴板");
+    if Clipboard::new()
+        .and_then(|mut c| c.set_text(&md))
+        .inspect_err(|e| eprintln!("写入剪贴板失败: {e}"))
+        .is_ok()
+    {
+        println!("已将代码块复制到剪贴板");
+    } else {
+        fs::write(current_dir.join("codemd.md"), &md).unwrap();
+    }
 }
